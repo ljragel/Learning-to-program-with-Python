8b05920d5785cf9954dc46d8abca7a99c91c9552
Marcos Medinilla <marcosmedinillahdez@gmail.com>
2018-07-09 09:59:53 +0100

    Adding an arithmetic average calculator

---
+++ arithmetic_average.py
@@ -0,0 +1,21 @@
+
+
+number_list = []
+loop_finalized = False
+
+while loop_finalized == False:
+    number = input("Que numero quieres introducir: ")
+    if not number == "fin":
+        number_list.append(int(number))
+    else:
+        loop_finalized = True
+
+list_len = len(number_list)
+
+total = 0
+for i in number_list:
+    total += i
+
+result = total / list_len
+
+print("La media de {} es {}".format(number_list, result))
