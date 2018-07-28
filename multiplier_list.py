7a5c730fbca65c1d8886819021a13c931474a6d4
Marcos Medinilla <marcosmedinillahdez@gmail.com>
2018-07-28 03:24:34 +0100

    Adding a new test with .replace changing vowels

---
+++ multiplier_list.py
@@ -0,0 +1,18 @@
+
+
+user_numbers = (input("Dime el número que deseas agregar a la lista: "))
+
+numbers_list = []
+
+while user_numbers != "FIN":
+
+        for user_numbers in numbers_list:
+        numbers_list.append(user_numbers)
+        print("¡Número añadido!")
+
+list_longitude = len(numbers_list)
+initial_index = 0
+
+print(list_longitude)
+
+
