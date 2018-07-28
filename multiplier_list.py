d280cdd727d1ad4dc1057499c126dc9b6c6ba35a
Marcos Medinilla <marcosmedinillahdez@gmail.com>
2018-07-28 12:57:42 +0100

    Improving

--- multiplier_list.py
+++ multiplier_list.py
@@ -1,16 +1,15 @@
 
 
-user_numbers = (input("Dime el número que deseas agregar a la lista: "))
+user_input = input("Dime el número que deseas agregar a la lista: ")
 
-numbers_list = []
+user_number_list = []
 
-while user_numbers != "FIN":
+while user_input != "FIN":
+    user_number_list.append(user_input)
+    print("¡Número añadido correctamente!")
+    user_input = input("Introduce un nuevo número para añadirlo: ").upper()
 
-        for user_numbers in numbers_list:
-        numbers_list.append(user_numbers)
-        print("¡Número añadido!")
-
-list_longitude = len(numbers_list)
+list_longitude = len(user_number_list)
 initial_index = 0
 
 print(list_longitude)
