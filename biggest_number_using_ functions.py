93be2a93cb327795c8d721fc8b96b218683ce632
Marcos Medinilla <marcosmedinillahdez@gmail.com>
2018-09-03 10:46:53 +0100

    Testing...

--- biggest_number_using_ functions.py
+++ biggest_number_using_ functions.py
@@ -5,18 +5,24 @@ Escribe un programa que encuentre el numero más grande entre 3 numeros
 
 input_numbers = input("Dame 3 números para poder realizar mi función (Introdúcelos separados por comas y espacio): ")
 
-def biggest_number():
-    current_index = 0
-    biggest_number = 0
+
+
+def biggest_number(input_numbers):
+    list = []
+    current_index = len(input_numbers) - 1
     while current_index <= 4:
-        if input_numbers[0] > 0:
-            biggest_number = input_numbers[0]
-            current_index += 1
+        first_number = list.append(input_numbers[0])
+        second_number = list.append(input_numbers[1])
+        third_number = list.append(input_numbers[2])
 
+    biggest_number = 0
+    for item in list:
+        if biggest_number < item:
+            biggest_number = item
 
-final = biggest_number(input_numbers)
+    return input_numbers
 
-print(final)
+print(biggest_number)
 
 
 
