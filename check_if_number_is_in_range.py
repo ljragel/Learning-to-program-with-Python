97c33c879cf27f2fe6888815b05553ec9f304be0
Marcos Medinilla <marcosmedinillahdez@gmail.com>
2018-09-05 11:44:22 +0100

    New program, still not working

---
+++ check_if_number_is_in_range.py
@@ -0,0 +1,24 @@
+
+"""
+Escribe una funcion que dado un numero y un rango (otros dos numeros), compruebe que ese numero está entre los dos (dentro del rango).
+"""
+#
+
+
+print("Este programa calcula si un número está dentro del rango de 0 a 100")
+
+number_to_check = int(input("Introduce el número que deseas comprobar si está dentro del rango de 0 a 100: "))
+
+def in_range (number_to_check):
+    if number_to_check >= 0 and number_to_check <= 100:
+        print("El número {} está dentro del rango".format(number_to_check))
+
+    else:
+        int(input("Introduce el número que deseas comprobar si está dentro del rango de 0 a 100: "))
+
+    return (number_to_check)
+
+in_range(print("El número {} está dentro del rango".format(number_to_check)))
+in_range(print("El número {} está dentro del rango".format(number_to_check)))
+
+
