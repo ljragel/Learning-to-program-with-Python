c6c294819713a2c530c0495322df8621865acb36
Marcos Medinilla <marcosmedinillahdez@gmail.com>
2018-09-05 10:54:16 +0100

    Finishing this program, but maybe It has bugs to fix

--- biggest_number_using_ functions.py
+++ biggest_number_using_ functions.py
@@ -3,28 +3,27 @@
 Escribe un programa que encuentre el numero más grande entre 3 numeros
 """
 
-#Da "error" (function biggest_number at 0x0159B618), no funciona del todo
+#Funciona pero no siempre correctamente a veces devuelve realmente el número mayor pero otras no (tiene un problema con el número 10) PD: Estaría bien utilizar una función propia en vez de max()
 
 
+print("¡Introduce 3 números para calcular el mayor de ellos!")
 
-input_numbers = input("Dame 3 números para poder realizar mi función (Introdúcelos separados por comas y espacio): ")
+numbers_list = []
+def security_answer(question):
+    if question.isdigit():
+        numbers_list.append(question)
+        print("¡Número añadido correctamente!")
 
-def biggest_number(input_numbers):
-    list = []
-    current_index = len(input_numbers) - 1
-    while current_index <= 4:
-        first_number = list.append(input_numbers[0])
-        second_number = list.append(input_numbers[1])
-        third_number = list.append(input_numbers[2])
 
-    biggest_number = 0
-    for item in list:
-        if biggest_number < item:
-            biggest_number = item
+question = security_answer(input("Inserte el primer número para añadirlo a la lista: "))
 
-    return input_numbers
+question = security_answer(input("Inserte el segundo número para añadirlo a la lista: "))
 
-print(biggest_number)
+question = security_answer(input("Inserte el último número para añadirlo a la lista: "))
+
+
+print("El número más grande de los tres es {}".format(max(numbers_list)))
+
 
 
 
