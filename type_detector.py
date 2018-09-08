1343ad89fafb6b0adb79eff89f51101787a21300
Marcos Medinilla <marcosmedinillahdez@gmail.com>
2018-09-08 02:47:58 +0100

    Nope

--- type_detector.py
+++ type_detector.py
@@ -1,9 +1,11 @@
 
-#Dada una lista mixta de enteros y strings, devolver dos listas, una con todos los enteros y otra con todas las strings.#
+"""
+Dada una lista mixta de enteros y strings, devolver dos listas, una con todos los enteros y otra con todas las strings.
+"""
 
 user_list = []
 
-user_input = (input("Dime un nuevo item para añadirlo en la lista (Introduce  'Fin' para finalizar la lista): "))
+user_input = (input("Dime un nuevo item para añadirlo en la lista (Introduce  'Fin' para finalizar la lista): ".upper()))
 
 while user_input != "FIN":
     user_list.append(user_input)
@@ -16,6 +18,3 @@ total_items = len(user_list)
 
 for new_item in user_list:
     if new_item == str:
-
-        else:
-
